26013a2042469611df3ac179a594d6616a406f7f
Jonathan-Frey <jf223rf@student.lnu.se>
2024-04-04 19:00:31 +0200

    second commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: false,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
