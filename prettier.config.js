acf7a62c7a1ec52724119818d1f70cb19080c14a
Jonathan-Frey <jf223rf@student.lnu.se>
2024-04-08 14:48:13 +0200

    added docker file and pipeline file

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   semi: false,
-  trailingComma: "all",
+  trailingComma: 'all',
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
