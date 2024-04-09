9ded01c137d86658bfa2994237d9cef246d326cc
Jonathan-Frey <jf223rf@student.lnu.se>
2024-04-09 08:45:10 +0200

    update pipeline config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 import { getServerAuthSession } from '@/server/auth'
-import { api } from '@/trpc/server'
+// import { api } from '@/trpc/server'
 
 export default async function Home() {
   const session = await getServerAuthSession()
